8307adcd7fc2b615af73dcf4414187a9d18ef4f6
Lior Bornshtain <lior.bornshtain@gmail.com>
2020-11-06 11:46:23 +0100

    adding web data model. close #2

---
+++ src/main/java/net/monga/micro/msscbeerservice/web/model/BeerPagedList.java
@@ -0,0 +1,19 @@
+package net.monga.micro.msscbeerservice.web.model;
+
+// This is a pagable object for our beer DTO
+
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public class BeerPagedList extends PageImpl<BeerDto> {
+
+    public BeerPagedList(List<BeerDto> content, Pageable pageable, long total) {
+        super(content, pageable, total);
+    }
+
+    public BeerPagedList(List<BeerDto> content) {
+        super(content);
+    }
+}
---
+++ src/main/java/net/monga/micro/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,5 @@
+package net.monga.micro.msscbeerservice.web.model;
+
+public enum BeerStyleEnum {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, IPA, PALE_ALE, SAISON
+}
